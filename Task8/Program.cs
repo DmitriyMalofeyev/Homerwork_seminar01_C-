fa18fda6d73675ef5e590af7818fe4831a7419cd
DmitriyMalofeyev <et5@list.ru>
2022-06-26 22:47:15 +0300

    корректировка случая N=1

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-Console.WriteLine("Введите целое число N");
+Console.WriteLine("Введите целое  натуральное число N");
 int N = Convert.ToInt32(Console.ReadLine());
 
 int index = 2;
@@ -9,4 +9,8 @@ while (index <= N)
 {
     Console.Write($"{index} ");
     index = index + 2;
+}
+if (N == 1)
+{
+    Console.WriteLine ("Четных чисел нет");
 }
